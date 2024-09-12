4ad554f749bee0d646d4c11b33d97ff70cbc7283
Sasanda Saumya <sasadasaumya@gmail.com>
2024-09-12 20:56:20 +0530

    add product

--- build/web/assets/requestJS/myAccount.js
+++ build/web/assets/requestJS/myAccount.js
@@ -6,14 +6,33 @@ const loadProductFeatures = async () => {
 
 
     if (response.ok) {
-        const json = response.json();
+        const json = await response.json();
 
         console.log(json);
+
+        loadSelectTag("category", json.categoryList, "name");
+        loadSelectTag("author", json.authorList, "name");
+        loadSelectTag("publisher", json.publisherList, "name");
+        loadSelectTag("status", json.statusList, "name");
+
+
 
     } else {
         console.log("Something went wrong");
 
     }
 
+
+};
+
+const loadSelectTag = (selectTagId, list, property) => {
+
+    const selectTag = document.getElementById(selectTagId);
+    list.forEach(item => {
+        let optionTag = document.createElement("option");
+        optionTag.value = item.id;
+        optionTag.innerHTML = item[property];
+        selectTag.appendChild(optionTag);
+    });
 
 };
