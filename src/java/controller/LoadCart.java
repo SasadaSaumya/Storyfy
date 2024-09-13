ea7d98c99a920219d84de7b4aafd691db749b525
Sasanda Saumya <sasadasaumya@gmail.com>
2024-09-13 19:11:42 +0530

    cart ok

--- src/java/controller/LoadCart.java
+++ src/java/controller/LoadCart.java
@@ -1,87 +1,94 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package controller;
 
+import com.google.gson.Gson;
+import dto.Cart_DTO;
+import dto.User_DTO;
+import entity.Cart;
+import entity.Product;
+import entity.User;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.HibernateUtil;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 /**
  *
- * @author sasad
+ * @author sasa
  */
 @WebServlet(name = "LoadCart", urlPatterns = {"/LoadCart"})
 public class LoadCart extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoadCart</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoadCart at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Gson gson = new Gson();
+
+        HttpSession httpSession = request.getSession();
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        ArrayList<Cart_DTO> cart_DTO_List = new ArrayList<>();
+
+        try {
+
+            if (httpSession.getAttribute("user") != null) {
+                //DB Cart
+
+                User_DTO user_DTO = (User_DTO) httpSession.getAttribute("user");
+
+                Criteria criteria1 = session.createCriteria(User.class);
+                criteria1.add(Restrictions.eq("email", user_DTO.getEmail()));
+                User user = (User) criteria1.uniqueResult();
+
+                Criteria criteria2 = session.createCriteria(Cart.class);
+                criteria2.add(Restrictions.eq("user", user));
+
+                List<Cart> cartList = criteria2.list();
+
+                for (Cart cart : cartList) {
+                    Cart_DTO cart_DTO = new Cart_DTO();
+
+                    Product product = cart.getProduct();
+                    product.setUser(null);
+                    cart_DTO.setProduct(product);
+
+                    cart_DTO.setQty(cart.getQty());
+
+                    cart_DTO_List.add(cart_DTO);
+
+                }
+
+            } else {
+
+                //Session Cart
+                if (httpSession.getAttribute("sessionCart") != null) {
+
+                    cart_DTO_List = (ArrayList<Cart_DTO>) httpSession.getAttribute("sessionCart");
+
+                    for (Cart_DTO cart_DTO : cart_DTO_List) {
+                        cart_DTO.getProduct().setUser(null);
+                    }
+                } else {
+                    //cart Empty
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    }
+        session.close();
+        response.setContentType("application/json");
+        response.getWriter().write(gson.toJson(cart_DTO_List));
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
